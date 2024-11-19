ea8a96882e37385f5cf3c0b1152613a8c8266c77
iveahugeship <iveahugeship@gmail.com>
2024-11-19 19:48:59 +0400

    comment ftype package

--- utils/file/ftype/ftype.go
+++ utils/file/ftype/ftype.go
@@ -4,6 +4,7 @@ import (
 	"io/fs"
 )
 
+// Predefined file types for easy identification of various file modes.
 var (
 	DevType       = NewFileType("DevType", fs.ModeDevice)
 	CharDevType   = NewFileType("CharDevType", fs.ModeCharDevice)
@@ -14,11 +15,18 @@ var (
 	RegularType   = NewFileType("RegularType", 0)
 )
 
+// FileType represents a specific type of file, defined by a name and a file mode.
 type FileType struct {
 	name string
 	mode fs.FileMode
 }
 
+// NewFileType creates a new instance of FileType with the provided name and mode.
+// Parameters:
+// - name: A string representing the name of the file type.
+// - mode: The fs.FileMode corresponding to the file type.
+// Returns:
+// - FileType: A new FileType instance.
 func NewFileType(name string, mode fs.FileMode) FileType {
 	return FileType{
 		name: name,
@@ -26,6 +34,11 @@ func NewFileType(name string, mode fs.FileMode) FileType {
 	}
 }
 
+// IsTypeOf checks if the provided file info matches the FileType's mode.
+// Parameters:
+// - info: An fs.FileInfo object containing information about a file.
+// Returns:
+// - bool: True if the file's mode matches the FileType's mode; otherwise, false.
 func (f FileType) IsTypeOf(info fs.FileInfo) bool {
 	return f.mode == info.Mode().Type()
 }
